4f5fdc61f85e97491888917094b86d1ecde8b358
Haritz <haritziturbe@gmail.com>
2013-10-16 12:03:44 +0200

    lehenengo bertsioa bukatuta

--- contact-form-submission.php
+++ contact-form-submission.php
@@ -2,7 +2,7 @@
 
 // check for form submission - if it doesn't exist then send back to contact form
 if (!isset($_POST['save']) || $_POST['save'] != 'contact') {
-    header('Location: contact.php'); exit;
+    header('Location: contacto.php'); exit;
 }
 	
 // get the posted data
@@ -13,39 +13,39 @@ $message = $_POST['contact_message'];
 	
 // check that a name was entered
 if (empty($name))
-    $error = 'You must enter your name.';
+    $error = 'Tienes que introducir un nombre.';
 // check that an email address was entered
 elseif (empty($email_address)) 
-    $error = 'You must enter your email address.';
+    $error = 'Tienes que introducir una dirección de email.';
 // check for a valid email address
 elseif (!preg_match('/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/', $email_address))
-    $error = 'You must enter a valid email address.';
+    $error = 'Tienes que introducir una dirección de email valida.';
 // check that a phone number was entered
 if (empty($phone))
-    $error = 'You must enter your phone number.';
+    $error = 'Tienes que introducir un número de teléfono.';
 // check that a message was entered
 elseif (empty($message))
-    $error = 'You must enter a message.';
+    $error = 'Tienes que introducir un mensaje.';
 		
 // check if an error was found - if there was, send the user back to the form
 if (isset($error)) {
-    header('Location: contact.php?e='.urlencode($error)); exit;
+    header('Location: contacto.php?e='.urlencode($error)); exit;
 }
 
 $headers = "From: $email_address\r\n"; 
 $headers .= "Reply-To: $email_address\r\n";
 
 // write the email content
-$email_content = "Name: $name\n";
-$email_content .= "Email Address: $email_address\n";
-$email_content .= "Phone Number: $phone\n";
-$email_content .= "Message:\n\n$message";
+$email_content = "Nombre: $name\n";
+$email_content .= "Email: $email_address\n";
+$email_content .= "Teléfono: $phone\n";
+$email_content .= "Mensaje:\n\n$message";
 	
 // send the email
 //ENTER YOUR INFORMATION BELOW FOR THE FORM TO WORK!
-mail ('YOUR-EMAIL-ADDRESS@YOUR-DOMAIN.com', 'YOUR WEBSITE NAME - Contact Form Submission', $email_content, $headers);
+mail ('haritziturbe@gmail.com', 'HIRUMET.com - Mensaje del formulario de Contacto', $email_content, $headers);
 	
 // send the user back to the form
-header('Location: contact.html?s='.urlencode('Thank you for your message.')); exit;
+header('Location: contacto.php?s='.urlencode('Gracias por enviar el mensaje.')); exit;
 
 ?>
--- contacto.php
+++ contacto.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -28,7 +28,7 @@
             <span class="icon-bar"></span>
           </button>
           <!-- You'll want to use a responsive image option so this logo looks good on devices - I recommend using something like retina.js (do a quick Google search for it and you'll find it) -->
-          <a class="navbar-brand" href="index.html">Hirumet</a>
+          <a class="navbar-brand" href="index.html"><img src="img/logo.png" alt=""></a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
@@ -50,7 +50,7 @@
       <div class="row">
       
         <div class="col-lg-12">
-          <h1 class="page-header">Contacto <small>Ponte en contacto con nosotros</small></h1>
+          <h1 class="page-header">Contacto</h1>
           <ol class="breadcrumb">
             <li><a href="index.html">Inicio</a></li>
             <li class="active">Contacto</li>
@@ -59,7 +59,7 @@
         
         <div class="col-lg-12">
           <!-- Embedded Google Map using an iframe - to select your location find it on Google maps and paste the link as the iframe src. If you want to use the Google Maps API instead then have at it! -->
-          <iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;ll=37.0625,-95.677068&amp;spn=56.506174,79.013672&amp;t=m&amp;z=4&amp;output=embed"></iframe>
+          <iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.es/maps?f=q&amp;source=s_q&amp;hl=es&amp;geocode=&amp;q=hirumet&amp;aq=&amp;sll=43.28441,-2.172193&amp;sspn=0.014214,0.031629&amp;t=m&amp;g=zarautz&amp;ie=UTF8&amp;ll=43.207801,-2.377536&amp;spn=0.18386,0.492824&amp;output=embed"></iframe><br /><small><a href="https://www.google.es/maps?f=q&amp;source=embed&amp;hl=eu&amp;geocode=&amp;q=hirumet&amp;aq=&amp;sll=43.28441,-2.172193&amp;sspn=0.014214,0.031629&amp;t=m&amp;g=zarautz&amp;ie=UTF8&amp;ll=43.207801,-2.377536&amp;spn=0.18386,0.492824" style="color:#0000FF;text-align:left">Ikusi mapa handiagoa</a></small>
         </div>
 
       </div><!-- /.row -->
@@ -67,8 +67,8 @@
       <div class="row">
 
         <div class="col-sm-8">
-          <h3>Let's Get In Touch!</h3>
-          <p>Lid est laborum dolo rumes fugats untras. Etharums ser quidem rerum facilis dolores nemis omnis fugats vitaes nemo minima rerums unsers sadips amets. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+          <h3>Ponte en contacto con nosotros:</h3>
+          <hr>
 			<?php  
 
                 // check for a successful form post  
@@ -80,39 +80,39 @@
 			?>
             <form role="form" method="POST" action="contact-form-submission.php">
               <div class="form-group col-lg-4">
-                <label for="input1">Name</label>
+                <label for="input1">Nombre</label>
                 <input type="text" name="contact_name" class="form-control" id="input1">
               </div>
               <div class="form-group col-lg-4">
-                <label for="input2">Email Address</label>
+                <label for="input2">Email</label>
                 <input type="email" name="contact_email" class="form-control" id="input2">
               </div>
               <div class="form-group col-lg-4">
-                <label for="input3">Phone Number</label>
+                <label for="input3">Teléfono</label>
                 <input type="phone" name="contact_phone" class="form-control" id="input3">
               </div>
               <div class="clearfix"></div>
               <div class="form-group col-lg-12">
-                <label for="input4">Message</label>
+                <label for="input4">Mensaje</label>
                 <textarea name="contact_message" class="form-control" rows="6" id="input4"></textarea>
               </div>
               <div class="form-group col-lg-12">
                 <input type="hidden" name="save" value="contact">
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Enviar</button>
               </div>
             </form>
         </div>
 
         <div class="col-sm-4">
-          <h3>Modern Business</h3>
-          <h4>A Start Bootstrap Template</h4>
+          <h3>Hirumet S.L.</h3>
+          <h4>Almacenes y Oficinas</h4>
           <p>
-            5555 44th Street N.<br>
-            Bootstrapville, CA 32323<br>
+            Ctra. Durango-Elorrio, s/n, ATXONDO<br>
+            48291 APATAMONASTERIO (Bizkaia)<br>
           </p>
-          <p><i class="icon-phone"></i> <abbr title="Phone">P</abbr>: (555) 984-3600</p>
-          <p><i class="icon-envelope-alt"></i> <abbr title="Email">E</abbr>: <a href="mailto:feedback@startbootstrap.com">feedback@startbootstrap.com</a></p>
-          <p><i class="icon-time"></i> <abbr title="Hours">H</abbr>: Monday - Friday: 9:00 AM to 5:00 PM</p>
+          <p><i class="icon-phone"></i> <abbr title="Phone">T</abbr>: 946 582412 - 946 582651 - 946 582701</p>
+          <p><i class="icon-envelope-alt"></i> <abbr title="Email">E</abbr>: <a href="mailto:feedback@startbootstrap.com">info@hirumet.com</a></p>
+          <p><i class="icon-time"></i> <abbr title="Hours">H</abbr>: Lunes - Viernes: 9:00 - 19:00</p>
           <ul class="list-unstyled list-inline list-social-icons">
             <li class="tooltip-social facebook-link"><a href="#facebook-page" data-toggle="tooltip" data-placement="top" title="Facebook"><i class="icon-facebook-sign icon-2x"></i></a></li>
             <li class="tooltip-social linkedin-link"><a href="#linkedin-company-page" data-toggle="tooltip" data-placement="top" title="LinkedIn"><i class="icon-linkedin-sign icon-2x"></i></a></li>
@@ -132,7 +132,7 @@
       <footer>
         <div class="row">
           <div class="col-lg-12">
-            <p>Copyright &copy; Company 2013</p>
+            <p>Copyright &copy; Hirumet 2013</p>
           </div>
         </div>
       </footer>
